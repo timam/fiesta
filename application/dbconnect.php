d4c383d2c43c4e79e577bca9795179e29b37bbc8
Shaiful Alam <timam@users.noreply.github.com>
2018-08-30 21:59:40 +0600

    testing with elb

--- application/dbconnect.php
+++ application/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
+    $servername = "swin-rds.coz5ywwvjdnp.us-east-1.rds.amazonaws.com";
+    $username = "foo";
+    $password = "foobarbaz";
 
     // Create connection
     $conn = mysqli_connect($servername, $username, $password);
