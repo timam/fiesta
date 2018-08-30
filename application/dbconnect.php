50d0f0d3cd703a637d109ad9c9bc6985ade88d18
Shaiful Alam <timam@users.noreply.github.com>
2018-08-30 22:17:29 +0600

    testing with swin db

--- application/dbconnect.php
+++ application/dbconnect.php
@@ -3,13 +3,14 @@
     $username = "foo";
     $password = "foobarbaz";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password);
-
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    echo "Connected successfully";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=swindb", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully"; 
+        }
+    catch(PDOException $e)
+        {
+        echo "Connection failed: " . $e->getMessage();
+        }
 ?>
-
